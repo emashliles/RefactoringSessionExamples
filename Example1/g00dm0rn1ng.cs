e3171eff1d12d9ff86c18d2ff67168dfb87495e7
emma-ashley.liles <emma-ashley.liles@7digital.com>
2014-09-23 17:56:07 +0100

    Hehehe

---
+++ Example1/g00dm0rn1ng.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+	class g00dm0rn1ng
+	{
+		static void Main(string[] args)
+		{
+			causeresharpertohaveafit(args[0]);
+		}
+
+		public static string m0rn1ng
+		{
+			get { return "Hello, "; }
+			set { }
+		}
+
+		public static void causeresharpertohaveafit(string whatthepersoniscalled)
+		{
+			Console.Write(m0rn1ng);
+			Console.Write(whatthepersoniscalled);
+			Console.ReadKey();
+
+		}
+
+		public static string AddASeparator(string text)
+		{
+			return text + " ";
+
+		}
+	}
+}
